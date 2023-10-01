b640561946fb86d2268b5cb01f713de0126f1c70
wangscs <steven14812@gmail.com>
2023-10-01 16:22:36 -0400

    Implement retrieving activities from API + Cors

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import {Header, List} from 'semantic-ui-react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  })
 
   return (
-    <h1>Reactivities</h1>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+    </div>
+    
   )
 }
 
