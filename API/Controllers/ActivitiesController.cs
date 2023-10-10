6f7ca3d69cf8d6e817cf3cd256084b25d3ed03ee
wangscs <steven14812@gmail.com>
2023-10-10 17:59:34 -0400

    Implement GetActivities and GetActivity Request

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,27 +1,21 @@
+using Application.Actvities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-            
-        }
-
         [HttpGet]   //{url}/api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _context.Activities.ToListAsync();
+            // return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //{url}/api/activities/123145
         public async Task<ActionResult<Activity>> GetActivity(Guid id){
-            return await _context.Activities.FindAsync(id);
+            //return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});
         }
     }
 }
