a0e89a0e32a3ef5a11fdda490468e682d41deee5
wangscs <steven14812@gmail.com>
2023-10-14 19:39:16 -0400

    Organize folders and added Activity Interface

--- Application/Actvities/List.cs
+++ Application/Actvities/List.cs
@@ -14,7 +14,7 @@ namespace Application.Actvities
             private readonly DataContext _context;
             public Handler(DataContext context)
             {
-            _context = context;
+                _context = context;
 
             }
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
