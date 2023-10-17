71de56d906f5ef0c7c1436aeaa64bccbda9e91b7
Steven <steven14812@gmail.com>
2023-10-17 15:29:59 -0400

    Added UI Activity Creation Form

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import { Grid, GridColumn, List } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
 
 interface Props {
     activities : Activity[];
@@ -17,6 +18,7 @@ export default function ActivityDashboard({activities}: Props) {
             <GridColumn width='6'>
                 {activities[0] &&
                 <ActivityDetails activity={activities[0]} />}
+                <ActivityForm />
             </GridColumn>
         </Grid>
     )
